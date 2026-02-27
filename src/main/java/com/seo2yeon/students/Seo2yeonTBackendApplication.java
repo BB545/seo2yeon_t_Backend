b96d32f05ca33ca798d00e3863a3f959fca4b667
BB545 <black1016@hanmail.net>
2026-02-27 19:46:49 +0900

    Feat: Spring Boot 프로젝트 기본 구조 생성

---
+++ src/main/java/com/seo2yeon/students/Seo2yeonTBackendApplication.java
@@ -0,0 +1,13 @@
+package com.seo2yeon.students;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seo2yeonTBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seo2yeonTBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seo2yeon/students/Seo2yeonTBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seo2yeon.students;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seo2yeonTBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
